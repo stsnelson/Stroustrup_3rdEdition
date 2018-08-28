0dd603f0dd83228c71438ee236a60b9b8739d412
Steve Snelson <stephen.snelson@outlook.com>
2018-08-28 23:14:24 +0100

    Chapter 4 ex 1 (build for linux)

---
+++ Chapter4/Ex1/HelloWorld.cpp
@@ -0,0 +1,18 @@
+/**
+* @file HelloWorld.cpp
+* @brief Chapter 4 Exercise 1.
+*
+* @details Get the "Hello World!" program to run. 
+*
+* @author Steve Snelson 
+*
+* @date 23/08/2018
+*/
+#include<iostream>
+
+int main()
+{
+	///Write out to screent "Hello World"
+	std::cout<< "Hello World!" << std::endl;
+	return(0);
+}
