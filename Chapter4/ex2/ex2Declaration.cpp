25504ce2f78d18c68317427fcfe063ceec82d32f
Steve Snelson <stephen.snelson@outlook.com>
2018-08-29 19:14:19 +0100

    Update draft ex2

---
+++ Chapter4/ex2/ex2Declaration.cpp
@@ -0,0 +1,21 @@
+
+
+char ch;
+string s;
+int count=1;
+const double pi = 3.1415926535897932385;
+extern int error_number;
+
+char* name="Njal";
+char* season[]={"spring","summer","autum","winter"};
+
+struct Date { int d,m,y;};
+int day(Date* p) {return p->d;};
+double sqrt(double);
+template<class T> T abs(T a) {return a<0 ? -a : a;};
+
+typedef complex<short> Point;
+struct User;
+enum Beer {Carlsberg, Thor, Tuborg};
+namespace NS(int a:);
+
